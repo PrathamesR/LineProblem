f01d54972b452c4a73b198c39ddf87f91d094892
Prathamesh Rajput <rajputprathamesh1998@gmail.com>
2020-09-27 21:24:09 +0530

    [Prathamesh Rajput] Add. Completed Use Case 3

--- Program.cs
+++ Program.cs
@@ -11,22 +11,27 @@ namespace LineProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation");
+            Console.WriteLine("Welcome to Line Comparison Computation\n");
 
             Line l1 = new Line();
             l1.SetLine();
             
             //Use Case 1
-            Console.WriteLine("Length of Line : "+ UseCase1.CalculateLength(l1));
+            Console.WriteLine("\nLength of Line : "+ UseCase1.CalculateLength(l1));
 
             //Use Case 2
+            Console.WriteLine();
             Line l2 = new Line();
             Console.WriteLine("\nFor Line 2: ");
             l2.SetLine();
             if (UseCase2.CheckEquality(l1, l2))
-                Console.WriteLine("The lines are equal");
+                Console.WriteLine("The lines are same");
             else
-                Console.WriteLine("The lines are unequal");
+                Console.WriteLine("The lines are different");
+
+            //Use Case 3
+            Console.WriteLine();
+            UseCase3.IdentifyGreater(l1, l2);
 
             //Temp Read line before exit
             Console.ReadLine();
---
+++ UseCases/UseCase3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineProblem.UseCases
+{
+    public class UseCase3
+    {
+        public static void IdentifyGreater(Line line1, Line line2)
+        {
+            if (UseCase1.CalculateLength(line1) > UseCase1.CalculateLength(line2))
+                Console.WriteLine("The First Line is Longer");
+            else if (UseCase1.CalculateLength(line1) < UseCase1.CalculateLength(line2))
+                Console.WriteLine("The Second Line is Longer");
+            else
+                Console.WriteLine("Both Lines are equal in length");
+        }
+    }
+}
